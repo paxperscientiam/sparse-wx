41352a0c2781f6b3c0af6b1a9fad181e8443b4cc
Chris <chrisdavidramos@gmail.com>
2019-01-07 14:32:58 -0500

    OK so i merged user preferences submit buttons into a single button

--- Code/Cards/UserCard/Sections/_Actions.ts
+++ Code/Cards/UserCard/Sections/_Actions.ts
@@ -4,23 +4,18 @@ function submitNameCallback(e) {
     const upk = dictionary.PROPS
     const name = e.formInput.user_name_key
 
+    if (!isSet(name)) {
+        return
+    }
+
     if (validateUserName(name) && name.length > 0) {
         Logger.log(`User's chosen name is ${name}.`)
         userProperties.setProperty(upk.USER.NAME, name)
-        return CardService.newActionResponseBuilder()
-            .setNavigation(CardService.newNavigation().popCard().updateCard(MainCard(dictionary).build()))
-            .setNotification(CardService.newNotification()
-                             .setType(CardService.NotificationType.INFO)
-                             .setText(`Hello ${name}, thanks for using SparseWX.`))
-            .setStateChanged(true)
-            .build()
+        return {
+            message: `Hello ${name}, thanks for using SparseWX.`,
+        }
     } else {
-        return CardService.newActionResponseBuilder()
-            .setNotification(CardService.newNotification()
-                             .setType(CardService.NotificationType.ERROR)
-                             .setText("Sorry, try a different name."))
-            .setStateChanged(false)
-            .build()
+        throw new Error("Sorry, try a different name.")
     }
 }
 
@@ -30,6 +25,10 @@ function submitAddressCallback(e) {
     const BRAND = dictionary.BRAND
     const address = e.formInput.user_address_key
 
+    if (!isSet(address)) {
+        return
+    }
+
     const isValid = validateMailingAddress(address)
     // if valid, retrieve address from storage
     if (isValid[0]) {
@@ -37,33 +36,17 @@ function submitAddressCallback(e) {
         const userCache = CacheService.getUserCache()
         userCache.removeAll(["wxRaw", "wx"])
         // successful validation means that lon,lat, and address properties defined
-        return CardService.newActionResponseBuilder()
-            .setNavigation(CardService
-                           .newNavigation()
-                           .popCard()
-                           .updateCard(MainCard(dictionary).build()))
-            .setNotification(CardService.newNotification()
-                             .setType(CardService.NotificationType.INFO)
-                             .setText(`Address set`))
-            .setStateChanged(false)
-            .build()
 
     } else {
-        let response = "Sorry, try a different address."
+        const response = "Sorry, try a different address."
         if (isValid[1] === "ZERO_RESULTS") {
-            response = "Sorry, that address wasn't found. Try something more specific."
+            throw new Error("Sorry, that address wasn't found. Try something more specific.")
         }
 
         if (isValid[1] === "UNSUPPORTED_REGION") {
-            response = `Sorry, ${BRAND.NAME} only supports results for the USA.`
+            throw new Error(`Sorry, ${BRAND.NAME} only supports results for the USA.`)
         }
-
-        return CardService.newActionResponseBuilder()
-            .setNotification(CardService.newNotification()
-                             .setType(CardService.NotificationType.ERROR)
-                             .setText(response))
-            .setStateChanged(false)
-            .build()
+        throw new Error(response)
     }
 }
 
@@ -73,13 +56,58 @@ function submitTemperatureUnitCallback(e) {
 
     const chosenTempUnit = e.formInputs.temperature_unit_list[0]
 
+    if (!isSet(chosenTempUnit)) {
+        return
+    }
+
     userProperties.setProperty(UPK.USER.TEMP_UNIT, chosenTempUnit)
+}
+
+function processUserPreferencesFormCallback(e) {
+    const dictionary = new Dictionary()
+    Logger.log(e.formInput)
+    try {
+        submitAddressCallback(e)
+    } catch (event) {
+        return CardService.newActionResponseBuilder()
+            .setNotification(CardService.newNotification()
+                             .setType(CardService.NotificationType.ERROR)
+                             .setText(event))
+            .setStateChanged(false)
+            .build()
+    }
+
+    try {
+        submitTemperatureUnitCallback(e)
+    } catch (event) {
+        return CardService.newActionResponseBuilder()
+            .setNotification(CardService.newNotification()
+                             .setType(CardService.NotificationType.ERROR)
+                             .setText(event))
+            .setStateChanged(false)
+            .build()
+    }
+
+    try {
+        submitNameCallback(e)
+    } catch (event) {
+        return CardService.newActionResponseBuilder()
+            .setNotification(CardService.newNotification()
+                             .setType(CardService.NotificationType.ERROR)
+                             .setText(event))
+            .setStateChanged(false)
+            .build()
+    }
 
     return CardService.newActionResponseBuilder()
+        .setNavigation(CardService
+                       .newNavigation()
+                       .popCard()
+                       .updateCard(MainCard(dictionary).build()))
         .setNotification(CardService.newNotification()
                          .setType(CardService.NotificationType.INFO)
-                         .setText("Set temperature unit"))
-        .setStateChanged(true)
+                         .setText(`Settings changed`))
+        .setStateChanged(false)
         .build()
 }
 
@@ -91,7 +119,6 @@ function goToUserCardCallback() {
         )
         .setStateChanged(false)
         .build()
-
 }
 
 function goToHomeCardCallback() {
--- Code/Cards/UserCard/Sections/_UserSection.ts
+++ Code/Cards/UserCard/Sections/_UserSection.ts
@@ -5,17 +5,11 @@ function UserSection() {
 
     const isCurrentTempUnitF = userProperties.getProperty(UPK.USER.TEMP_UNIT) === "dropdown_item_f"
 
-    const submitNameAction =  CardService.newAction()
-        .setFunctionName("submitNameCallback")
-
-    const submitAddressAction = CardService.newAction()
-        .setFunctionName("submitAddressCallback")
-
     const submitAddressSuggestionsAction = CardService.newAction()
         .setFunctionName("getAddressSuggestionsCallback")
 
-    const submitTemperatureUnitAction = CardService.newAction()
-        .setFunctionName("submitTemperatureUnitCallback")
+    const processUserFormAction =  CardService.newAction()
+        .setFunctionName("processUserPreferencesFormCallback")
 
     const COLORS = dictionary.UI.PALETTE
 
@@ -25,11 +19,6 @@ function UserSection() {
                    .setTitle("Your Name (optional)")
                    .setHint("How should SparseWx address you? (25 character max)"))
 
-        .addWidget(CardService
-                   .newTextButton()
-                   .setText(`<font color="${COLORS.ORANGE}">[ Set username ]</font>`)
-                   .setOnClickAction(submitNameAction))
-
         .addWidget(CardService.newTextInput()
                    .setFieldName("user_address_key")
                    .setTitle("Forecast Location")
@@ -37,17 +26,16 @@ function UserSection() {
                    .setSuggestionsAction(submitAddressSuggestionsAction))
 
         .addWidget(CardService
-                   .newTextButton()
-                   .setText(`<font color="${COLORS.ORANGE}">[ Set location ]</font>`)
-                   .setOnClickAction(submitAddressAction))
-
-        .addWidget(CardService
                    .newSelectionInput()
                    .setType(CardService.SelectionInputType.DROPDOWN)
-                   .setTitle("List")
+                   .setTitle("Temperature Scale")
                    .setFieldName("temperature_unit_list")
                    .addItem("Fahrenheit", "dropdown_item_f", isCurrentTempUnitF)
-                   .addItem("Celsius", "dropdown_item_c", !isCurrentTempUnitF)
-                   .setOnChangeAction(submitTemperatureUnitAction))
+                   .addItem("Celsius", "dropdown_item_c", !isCurrentTempUnitF))
+
+        .addWidget(CardService
+                   .newTextButton()
+                   .setText(`<font color="${COLORS.ORANGE}">[ Submit ]</font>`)
+                   .setOnClickAction(processUserFormAction))
         .setCollapsible(false)
 }
