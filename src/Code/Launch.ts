27ab7cc21f9985821ef0790de122a66ef5003ba4
Chris <7539871+paxperscientiam@users.noreply.github.com>
2019-12-30 16:51:43 -0500

    update deps

--- src/Code/Launch.ts
+++ src/Code/Launch.ts
@@ -3,7 +3,11 @@
   checkLocationServiceStatus
 */
 
-function evalThis(code) {
+function six() {
+  return 66666
+}
+
+function evalThis(code: any) {
   // tslint:disable-next-line
   return eval(code)
 }
